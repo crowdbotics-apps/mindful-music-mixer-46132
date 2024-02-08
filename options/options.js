f513198f164c6b1da7a5e08d589eaf1aa1f7ee2a
Crowdbotics <team@crowdbotics.com>
2024-02-08 16:21:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindful-music-mixer-46132",
+  url: "https://mindful-music-mixer-46132.botics.co",
+  api: "https://mindful-music-mixer-46132.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
